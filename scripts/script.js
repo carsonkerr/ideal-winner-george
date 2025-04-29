b6920a3109b3740ce536302c8dab92611e7a7bb3
carsonkerr <167832137+carsonkerr@users.noreply.github.com>
2025-04-29 16:44:56 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+
+// $(document).ready(function(){
+  
+  
+//     $("#saveParking").on("click", function(){
+  
+//       let statusText = underConstruction("not ready");      
+//       $("#saveParking").text(statusText).prop(   "disabled", true);
+      
+//     });
+     
+//     function underConstruction(status){
+//       return "Not Yet Ready"
+//     };
+//   }
+// );
